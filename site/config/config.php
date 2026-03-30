21ca3279ba533fb76321899e0f8539f4c548e33b
Roberto Enrique Briceno Garcia <protorob@gmail.com>
2026-03-30 16:37:09 +0200

    pannel install for remote

--- site/config/config.php
+++ site/config/config.php
@@ -4,6 +4,9 @@ return [
     'debug'  => true,
     // Set to true to skip sending and write emails to logs/email-debug.log instead
     'fotoalbum.email.debug' => true,
+    'panel' =>[
+        'install' => true
+    ]
 
     // Email transport — switch to 'smtp' and fill credentials for production
     'email' => [
